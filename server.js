f1704d5b02c16e705155aaad300be352b9e6d9ff
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2023-09-23 00:04:23 +0700

    Added EJS Dependency & Changed index.ejs

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
   console.log('Server is running')
-  res.json({mssg: "Halo GAMADA 2023"})
-  res.render('index')
+  // res.json({mssg: "Halo GAMADA 2023"})
+  res.render('index', { name: "User" })
 })
 
 app.listen(3000, () => {
