e8b28e9aac3667e598bfcfddcf2e18876b84cffe
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2023-09-23 00:24:28 +0700

    Added users,js example

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,35 @@ router.get('/new', (req, res) => {
     res.send('new user')
 })
 
+router.post('/', (req, res) => {
+    res.send('create user')
+})
+
+// Router with ID Parameter
+router.route('/:id')
+    .get((req, res) => {
+        // res.send(`get user ${req.params.id}`)
+        res.send(`get user ${req.user.name}`)
+    })
+    .put((req, res) => {
+        res.send(`update user ${req.params.id}`)
+    })
+    .delete((req, res) => {
+        res.send(`delete user ${req.params.id}`)
+    })
+
+const dummyUsers = [
+    { id: 1, name: 'John' },
+    { id: 2, name: 'Doe' },
+    { id: 3, name: 'Smith' }
+]
+
+// Middleware for ID Parameter
+router.param('id', (req, res, next, id) => {
+    console.log(`User ${id} is found`)
+    req.user = dummyUsers.find(user => user.id === Number(id))
+    next()  // next() is required to continue the process
+})
+
+
 module.exports = router
