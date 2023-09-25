4f87a2350a7b0ea16d7547c35df9aa9ed85f4e4f
Giga Hidjrika Aura Adkhy <52191436+gigahidjrikaaa@users.noreply.github.com>
2023-09-25 10:43:29 +0700

    Fixed Users GET method

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,13 @@ const express = require('express')
 const router = express.Router()
 const Users = require('../models/usersModel')
 
-router.get('/', (req, res) => {
-    res.send('all users')
+router.get('/', async (req, res) => {
+    try {
+        const users = await Users.find()
+        res.status(200).json(users)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 })
 
 router.get('/new', (req, res) => {
