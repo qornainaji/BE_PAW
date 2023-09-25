6171498ceca562e77a398e0f096aa02fe2403e9d
Giga Hidjrika Aura Adkhy <52191436+gigahidjrikaaa@users.noreply.github.com>
2023-09-25 09:43:38 +0700

    Removed Logger function

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 
-app.use(logger)
-
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
