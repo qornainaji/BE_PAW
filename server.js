ff5b69aff41f0391f58d52b7ce3ba8fa79ac3726
AhmadFauzan19 <68361425+AhmadFauzan19@users.noreply.github.com>
2023-09-25 09:29:24 +0700

    DB: Initialize connection to MongoDB

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-const express = require('express')
+require('dotenv').config()
 
+const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 app.get('/', (req, res) => {
     res.json({mssg: "Halo GAMADA 2023"})
 })
 
-app.listen(4000, () => {
-  console.log('halo 4000')
-})
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(4000, () => {
+      console.log('halo 4000')
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+// PORT=4000
+// MONGO_URI=mongodb+srv://ahmadfauzan190603:pojanPAW@academiateti.snpla5s.mongodb.net/?retryWrites=true&w=majority
