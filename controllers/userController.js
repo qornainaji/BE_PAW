755db8891d97ef59b8540978a0864d625830a9cf
Giga Hidjrika Aura Adkhy <52191436+gigahidjrikaaa@users.noreply.github.com>
2023-11-15 23:26:51 +0700

    feat: added pagination middleware

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,7 @@ const jwt = require('jsonwebtoken');
 // get all user
 const getUsers = async (req, res) => {
     const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 5;
+    const limit = parseInt(req.query.limit) || 12;
     const startIndex = (page - 1) * limit;
     const endIndex = page * limit;
 
